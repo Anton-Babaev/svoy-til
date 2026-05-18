fec993c3b72f75f634780669326df9936e6ba80a
Антон Бабаев <climb.moscow@gmail.com>
2026-05-19 00:22:16 +0300

    Sprint 1: Home page with placeholders

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Главная страница с блоками-заглушками"""
+    context = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/home.html', context)
+
+
+def about(request):
+    """Временная страница 'О нас'"""
+    return render(request, 'main/about.html', {'title': 'О нас'})
